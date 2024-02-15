aabe95929cb6a63149afd2c4d6244370d665c36e
takenokao <153838584+takenokao@users.noreply.github.com>
2024-02-16 04:20:34 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
-$(document).ready(function() {
-  $(".audio_button").on("click", function () {
-      var audio = $("#audio")[0]; // jQueryを使用してオーディオ要素を取得します
-      if (audio.paused) {
-          audio.play();
-          $(this).toggleClass("stop");
-      } else {
-          audio.pause();
-          $(this).toggleClass("stop");
-      }
-  });
+document.addEventListener("DOMContentLoaded", function() {
+    var audioButton = document.querySelector(".audio_button");
+
+    audioButton.addEventListener("click", function() {
+        // ボタンのクラスを切り替える
+        audioButton.classList.toggle("stop");
+    });
 });
